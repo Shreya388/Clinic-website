206529453e843922c552bd7eab53a40173496195
Shreya388 <meenaxjha@gmail.com>
2023-03-21 13:54:06 +0530

    make the header green themed and changed header image

--- pages/index.js
+++ pages/index.js
@@ -57,12 +57,93 @@ export default function Home() {
           src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
         ></script>
 
-        {/* <div className={styles.header} alt="cover">
+<nav className="p-8 bg-white md:flex md:items-center md:justify-between sticky">
+  
+          <div className="flex justify-between items-center mx-auto">
+            <span className="text-2xl font-[Poppins] px-12 text-center">LOGO</span>
+          <div className="container">
+            
+
+            <span
+              onClick={() => setOpen(!open)}
+              className="text-3xl cursor-pointer md:hidden block text-right"
+            >
+              <ion-icon name={open ? "close" : "menu"}></ion-icon>
+            </span>
+          </div>
+          <ul
+            id="linkItems"
+            className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static mx-auto bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
+              open ? "top-[100px]" : "hidden"
+            }`}
+          >
+            <li className="mx-2 my-7  md:my-0">
+              <a
+                href="#"
+                className="text-white text-md hover:text-white duration-500 bg-green-600 rounded p-4"
+              >
+                HOME
+              </a>
+            </li>
+            <li className="mx-2 my-7 md:my-0">
+              <a
+                href="#"
+                className="text-md text-md hover:bg-green-600 hover:text-white rounded p-4 duration-300"
+              >
+                SERVICES
+              </a>
+            </li>
+            <li className="mx-2 my-7 md:my-0">
+              <a
+                href="#"
+                className="text-md text-md hover:bg-green-600 hover:text-white rounded p-4 duration-300"
+              >
+                ABOUT
+              </a>
+            </li>
+            <li className="mx-2 my-7 md:my-0">
+              <a
+                href="#"
+                className="text-md text-md hover:bg-green-600 hover:text-white rounded p-4 duration-300"
+              >
+                EQUIPMENT
+              </a>
+            </li>
+            <li className="mx-2 my-7 md:my-0">
+              <a
+                href="#"
+                className="text-md text-md hover:bg-green-600 hover:text-white rounded p-4 duration-300"
+              >
+                SPECIALISTS
+              </a>
+            </li>
+            <li className="mx-2 my-7 md:my-0">
+              <a
+                href="#"
+                className="text-md text-md hover:bg-green-600 hover:text-white rounded p-4 duration-300"
+              >
+                TESTIMONIALS
+              </a>
+            </li>
+            <li className="mx-2 my-7 md:my-0">
+              <a
+                href="#"
+                className="text-md text-md hover:bg-green-600 hover:text-white rounded p-4 duration-300"
+              >
+                CONTACT
+              </a>
+            </li>
+          </ul>
+          </div>
+        </nav>
+
+        <div className={styles.header} alt="cover">
+          <div className={styles.coverOverlay}>
           <div
-            className="container mx-auto py-52"
+            className="container mx-auto py-52 px-10"
             style={{ zIndex: "-1 !important" }}
           >
-            <div className=" ">
+            <div className="p-12">
               <h1
                 className={`${styles.headText} my-6 text-2xl text-white font-light`}
               >
@@ -71,134 +152,16 @@ export default function Home() {
               <h1 className="text-4xl text-white font-black">
                 We Offer Comprehensive Eye Exams
               </h1>
-              <button className="bg-blue-500 my-6 py-3 px-8 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-full">
+              <button className="bg-green-700 my-6 py-4 px-8 hover:bg-green-800 text-white font-medium py-2 px-4 rounded">
                 Explore Services
               </button>
             </div>
           </div>
-        </div> */}
-      
-      <div className="max-w-[100%] h-[580px] w-full m-auto relative group">
-        <nav className="p-8 bg-white md:flex md:items-center md:justify-between sticky">
-          <div className="flex justify-between items-center">
-            <span className="text-2xl font-[Poppins] px-12 text-center">LOGO</span>
-
-            <span
-              onClick={() => setOpen(!open)}
-              className="text-3xl cursor-pointer md:hidden block"
-            >
-              <ion-icon name={open ? "close" : "menu"}></ion-icon>
-            </span>
-          </div>
-          <ul
-            id="linkItems"
-            className={`md:flex px-20 md:items-center md:pb-0 pb-12 absolute md:static mx-auto bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
-              open ? "top-[100px]" : "hidden"
-            }`}
-          >
-            <li className="mx-4 my-7 md:my-0">
-              <a
-                href="#"
-                className="text-cyan-700 text-md text-sm hover:text-cyan-500 duration-500"
-              >
-                HOME
-              </a>
-            </li>
-            <li className="mx-4 my-7 md:my-0">
-              <a
-                href="#"
-                className="text-md text-sm hover:text-cyan-500 duration-500"
-              >
-                SERVICES
-              </a>
-            </li>
-            <li className="mx-4 my-7 md:my-0">
-              <a
-                href="#"
-                className="text-md text-sm hover:text-cyan-500 duration-500"
-              >
-                ABOUT
-              </a>
-            </li>
-            <li className="mx-4 my-7 md:my-0">
-              <a
-                href="#"
-                className="text-md text-sm hover:text-cyan-500 duration-500"
-              >
-                EQUIPMENT
-              </a>
-            </li>
-            <li className="mx-4 my-7 md:my-0">
-              <a
-                href="#"
-                className="text-md text-sm hover:text-cyan-500 duration-500"
-              >
-                SPECIALISTS
-              </a>
-            </li>
-            <li className="mx-4 my-7 md:my-0">
-              <a
-                href="#"
-                className="text-md text-sm hover:text-cyan-500 duration-500"
-              >
-                TESTIMONIALS
-              </a>
-            </li>
-            <li className="mx-4 my-7 md:my-0">
-              <a
-                href="#"
-                className="text-md text-sm hover:text-cyan-500 duration-500"
-              >
-                CONTACT
-              </a>
-            </li>
-          </ul>
-        </nav>
-        <div
-          style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
-          className="w-full h-full bg-center bg-cover duration-500"
-        >
-           <div
-            className="container mx-auto py-48"
-          >
-            <div className="text-white">
-              <h1
-                className={`${styles.headText} my-6 text-2xl text-blue font-light`}
-              >
-                At our eye care clinic
-              </h1>
-              <h1 className="text-5xl text-neutral-50 font-black text-black">
-                We Offer Comprehensive<br /> Eye Exams
-              </h1>
-              <button className="bg-blue-500 my-6 py-3 px-8 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-full">
-                Explore Services
-              </button>
-            </div>
           </div>
         </div>
-       
-        {/* Left Arrow */}
-        <div className="hidden group-hover:block absolute top-[65%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
-          <BsChevronCompactLeft onClick={prevSlide} size={30} />
-        </div>
-        {/* Right Arrow */}
-        <div className="hidden group-hover:block absolute top-[65%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
-          <BsChevronCompactRight onClick={nextSlide} size={30} />
-        </div>
-        <div className="flex top-4 justify-center py-2">
-          {slides.map((slide, slideIndex) => (
-            <div
-              key={slideIndex}
-              onClick={() => goToSlide(slideIndex)}
-              className="text-2xl cursor-pointer"
-            >
-              <RxDotFilled />
-            </div>
-          ))}
-        </div>
-      </div>
-      <AboutUs />
-      </div>
+          <AboutUs />
+        
+       </div>
 
       
     </>
